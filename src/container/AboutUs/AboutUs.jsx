e2980fa33460d21d66bafee66b08051dd0e22e22
EmP33 <purwinmarek16@gmail.com>
2022-04-03 19:06:21 +0200

    Add aboutus section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from "react";
+import "./AboutUs.css";
 
-import './AboutUs.css';
+import { images } from "../../constants";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+          fugit impedit quisquam quam, iusto amet non pariatur, corrupti ad
+          facere alias, tenetur nam rerum distinctio.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+          fugit impedit quisquam quam, iusto amet non pariatur, corrupti ad
+          facere alias, tenetur nam rerum distinctio.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
