0920b0aadb9ce480ddd3ab9f5da59de491c3feff
EmP33 <purwinmarek16@gmail.com>
2022-04-03 22:10:21 +0200

    Add chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
 import "./Chef.css";
 
-const Chef = () => <div>Chef</div>;
+import { images } from "../../constants";
+
+import { SubHeading } from "../../components";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Taque est, sunt eligendi delectus. Eos, non sed.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolores
+          dolorum enim debitis harum, consequatur voluptates voluptas fuga
+          libero ad pariatur possimus est. Mollitia, itaque.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
 
 export default Chef;
