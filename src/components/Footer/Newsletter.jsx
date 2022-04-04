461bf462189c95a8bef9c50e57b33511276e85ac
EmP33 <purwinmarek16@gmail.com>
2022-04-04 10:12:22 +0200

    Add footer section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Newsletter.css";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Latest Updates</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { SubHeading } from "../../components";
-import { images, data } from "../../constants";
+import { images } from "../../constants";
 
 const FindUs = () => (
   <div className="app__bg app__wrapper section__padding" id="contact">
